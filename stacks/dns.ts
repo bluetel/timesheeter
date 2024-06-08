433397791134fe3cecf40cdc8ba11271708fda70
Harry Twigg <harrytwigg111@gmail.com>
2024-06-08 11:58:15 +0100

    TS-53: Supply new hosted zone

--- stacks/dns.ts
+++ stacks/dns.ts
@@ -3,11 +3,8 @@ import { HostedZone } from 'aws-cdk-lib/aws-route53';
 import { sstEnv } from './lib';
 
 export function Dns({ stack, app }: StackContext) {
-  // capitalise the first letter of the stage
-  const appStageFormatted = `${app.stage[0].toUpperCase()}${app.stage.slice(1)}`;
-
   // Import the hosted zone from the aws account
-  const hostedZone = HostedZone.fromLookup(stack, `HostedZone${appStageFormatted}`, {
+  const hostedZone = HostedZone.fromLookup(stack, "HostedZone" {
     domainName: sstEnv.HOSTED_ZONE,
   });
 
