65d74ac975bce95e5fa240879e306528479f5f16
Harry Twigg <harrytwigg111@gmail.com>
2024-06-09 09:16:11 +0100

    TS-53-a: Add hosted zone id

--- stacks/dns.ts
+++ stacks/dns.ts
@@ -4,9 +4,7 @@ import { sstEnv } from './lib';
 
 export function Dns({ stack }: StackContext) {
   // Import the hosted zone from the aws account
-  const hostedZone = HostedZone.fromLookup(stack, 'HostedZone', {
-    domainName: sstEnv.HOSTED_ZONE,
-  });
+  const hostedZone = HostedZone.fromHostedZoneId(stack, 'HostedZone', sstEnv.HOSTED_ZONE_ID);
 
   const fqdn = `${sstEnv.APP_SUBDOMAIN_PARTS}.${hostedZone.zoneName}`;
 
